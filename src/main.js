3ea436229bc7caa0130f3c43b74d2af2f6562b13
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2018-06-08 17:34:57 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import {Time} from './time';
+require('style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css');
+require('bootstrap');
+
+new Vue({
+  el: '#app',
+  data: {
+    times: [
+      new Time("America MG", require('./assets/america_mg_60x60.png')),
+      new Time("Atlético PR", require('./assets/atletico-pr_60x60.png')),
+      new Time("Atlético MG", require('./assets/atletico_mg_60x60.png')),
+
+    ],
+    alfabeto: {
+      a: 'A',
+      b: 'B',
+      c: 'C',
+      d: 'D',
+      e: 'E',
+    }
+  },
+});
